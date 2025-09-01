5dad30fbf4d6a7783778c094866f39f881b3f70b
Marton Szep <marton.szep@tum.de>
2025-09-02 00:29:05 +0200

    no screen

--- start_babymonitor.sh
+++ start_babymonitor.sh
@@ -9,8 +9,12 @@ APP_PATH="/home/martons/raspberrypi-babycam"
 SCREEN_NAME="babymonitor"
 
 # Activate venv and start app in a detached screen
-screen -dmS $SCREEN_NAME bash -c "
-    source $VENV_PATH/bin/activate
-    cd $APP_PATH
-    python run.py
-"
+# screen -dmS $SCREEN_NAME bash -c "
+#     source $VENV_PATH/bin/activate
+#     cd $APP_PATH
+#     python run.py
+# "
+
+source $VENV_PATH/bin/activate
+cd $APP_PATH
+python run.py
