da57d5cb27a2cef0c59888dd53228b8e790e710f
Marton Szep <marton.szep@tum.de>
2025-09-02 00:21:25 +0200

    shell start script

---
+++ start_babymonitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Path to your venv
+VENV_PATH="/home/martons/raspberrypi-babycam/babycam_venv"
+# Path to your Flask app
+APP_PATH="/home/martons/raspberrypi-babycam"
+
+# Name of the screen session
+SCREEN_NAME="babymonitor"
+
+# Activate venv and start app in a detached screen
+screen -dmS $SCREEN_NAME bash -c "
+    source $VENV_PATH/bin/activate
+    cd $APP_PATH
+    python run.py
+"
